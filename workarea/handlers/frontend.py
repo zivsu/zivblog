e3759374cedb380710fcd143f459e54ecd6503fe
szm0816 <suwen2006@gmail.com>
2016-04-27 01:50:54 +0800

    fix post comment validate code bug

--- workarea/handlers/frontend.py
+++ workarea/handlers/frontend.py
@@ -134,13 +134,13 @@ class AddCommentHandler(FrontEndHandler):
 
         code_id = self.get_secure_cookie("codeid", None)
         if code_id is None:
-            return self.write({"err":True, "msg":u"验证码过期"})
+            return self.write({"err":True, "msg":u"无效的请求"})
 
         session = m_session.query_code(self.db, code_id)
         logging.info("session:{}".format(session))
         if session is None:
             logging.info("session expired")
-            return self.write({"err":True, "msg":u"无效的请求", "refresh":True})
+            return self.write({"err":True, "msg":u"验证码过期", "refresh":True})
 
         if session["code"] != validate_code:
             return self.write({"err":True, "msg":u"验证码错误"})
